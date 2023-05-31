9485593c543f2eb4339d04490386b0f63fd57855
mistik9 <mistik9@icloud.com>
2023-05-31 19:41:17 +0600

    add spinner

--- src/App.js
+++ src/App.js
@@ -2,79 +2,58 @@ import React from "react";
 import { Route, Routes, Navigate, useNavigate } from "react-router-dom";
 import Login from "./Login.js";
 import auth from "./utils/auth.js";
-import { Spinner } from "./Spinner.js";
+import ClipLoader from "react-spinners/ClipLoader";
 
-function App( {onLogin }) {
-    const [currentUser, setCurrentUser] = React.useState({});
-    const [loggedIn, setLoggedIn] = React.useState(false);
-    const [email, setEmail] = React.useState("");
-    const [userData, setUserData] = React.useState({});
-    const [password, setPassword] = React.useState("");
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      onLogin({ email, password })
+function App() {
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [infoMessage, setInfoMessage] = React.useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  //авторизация
+  function handleLogin({ email, password }) {
+    auth.authorize(email, password)
+    setIsLoading(true)
+      .then((res) => {
+        setLoggedIn(true);
+        setEmail(email);
+        setInfoMessage({ message: "Активен" });
+        setIsLoading(false) 
+      })
+      .catch((err) => {
+        console.log("Ошибка авторизации")
+        setIsLoading(false) 
+      })
   }
-  
-  function handleChangeEmail(e) {
-      setEmail(e.target.value);
+
+  //проверка авторизации
+
+  function handleAuthCheck() {
+    auth.hasAuth()
+      .then((res) => {
+        setLoggedIn(true);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
-  function handleChangePassword(e) {
-      setPassword(e.target.value);
-  }
 
-    //авторизация
-    function handleLogin({ email, password }) {
-        auth.authorize(email, password)
-            .then((res) => {
-                if (res.token) localStorage.setItem("token", res.token);
-                setLoggedIn(true);
-
-                setUserData(res.user);
-                setEmail(email);
-            })
-            .catch((err) => {
-                console.log("Ошибка авторизации")
-            })
-    }
-
-    //проверка токена
-    function handleAuthCheck() {
-        auth.hasAuth()
-            .then((res) => {
-                if (res) {
-                    setEmail(res.data.email)
-                    setLoggedIn(true);
-                }
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-    }
-
-
-return (
+  return (
     <div class="page">
-    <div class="alert ">{email}
-    <p class =""></p>
-    </div>
-    <form class="form" onSubmit={handleSubmit}>
-      <label class="form__label">Номер телефона или e-mail</label>
-      <input id="login" class="form__input " required  value={email} onChange={handleChangeEmail}/>
-        <label class="form__label">Пароль</label>
-        <input id="password" type="password" class="form__input " required  value={password} onChange={handleChangePassword}/>
-          <div>
-            <button class="form__button form__button_active">ПОДКЛЮЧИТЬ</button>
-            <button class="form__button">ОТМЕНА</button>
-          </div>
-        </form>
-        <div>
-          <p class="text">Для получения помощи перейдите в мессенджер:</p>
-          <button class="form__button">WHATSAPP</button>
-          <button class="form__button">TELEGRAM</button>
-        </div>
+      <div class="user">
+        <p class="user__email">{email}</p>
+        <p class="user__status">{infoMessage}</p>
+        <div className={`spinner ${isLoading ? 'spinner_active' : ''}`}></div>
       </div>
+      <Login onLogin={handleLogin} />
+      <div>
+        <p class="text">Для получения помощи перейдите в мессенджер:</p>
+        <button class="form__button" aria-label="WhatsApp">WHATSAPP</button>
+        <button class="form__button" aria-label="Telegram">TELEGRAM</button>
+      </div>
+    </div>
 
-)
+  )
 }
 export default App;
--- src/Login.js
+++ src/Login.js
@@ -8,34 +8,27 @@ function Login({ onLogin }) {
   const handleSubmit = (e) => {
     e.preventDefault();
     onLogin({ email, password })
-}
+  }
 
-function handleChangeEmail(e) {
+  function handleChangeEmail(e) {
     setEmail(e.target.value);
-}
-function handleChangePassword(e) {
+  }
+  function handleChangePassword(e) {
     setPassword(e.target.value);
-}
+  }
   return (
-    <div class="page">
-      <div class="alert "></div>
       <form class="form" onSubmit={handleSubmit}>
         <label class="form__label">Номер телефона или e-mail</label>
-        <input id="login" class="form__input " required  value={email} onChange={handleChangeEmail}/>
-          <label class="form__label">Пароль</label>
-          <input id="password" type="password" class="form__input " required  value={password} onChange={handleChangePassword}/>
-            <div>
-              <button class="form__button form__button_active">ПОДКЛЮЧИТЬ</button>
-              <button class="form__button">ОТМЕНА</button>
-            </div>
-          </form>
-          <div>
-            <p class="text">Для получения помощи перейдите в мессенджер:</p>
-            <button class="form__button">WHATSAPP</button>
-            <button class="form__button">TELEGRAM</button>
-          </div>
+        <input id="login" class="form__input " required value={email} onChange={handleChangeEmail} />
+        <label class="form__label">Пароль</label>
+        <input id="password" type="password" class="form__input " required value={password} onChange={handleChangePassword} />
+        <div>
+          <button class="form__button form__button_active">ПОДКЛЮЧИТЬ</button>
+          <button class="form__button">ОТМЕНА</button>
         </div>
-        );
+      </form>
+
+  );
 }
 
-        export default Login;
+export default Login;
