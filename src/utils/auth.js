a79d7ebeee9b79292e5be435b4cacecb0ee3325a
mistik9 <mistik9@icloud.com>
2023-07-01 11:35:16 +0600

    fix hasauth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -82,7 +82,7 @@ class Auth {
 
 const auth = new Auth({
     baseUrl: "https://autoparts-base.ru",
-   member_id: 'bcc2aa0d98a8c7155d417e06b8de7830',
-    // member_id: member_id
+    //    member_id: 'bcc2aa0d98a8c7155d417e06b8de7830',
+    member_id: member_id
 });
 export default auth;
